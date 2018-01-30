4891d2c70ddef1bbc1e53e9aac217230a7691067
Shota Suzuki <aqr.aqua@gmail.com>
2018-01-30 10:12:47 +0900

    Add ci_check.sh

---
+++ .circleci/ci_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+PROJECT_DIR=${SCRIPT_DIR}/../
+
+find . -maxdepth 1 -type d \
+       -not -path '*/\.*' \
+       -exec digdag check --project {} \;
