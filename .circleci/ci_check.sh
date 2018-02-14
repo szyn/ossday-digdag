cb6d31ddf3777c6e16721713b8188a5d3776c1ae
Shota Suzuki <aqr.aqua@gmail.com>
2018-02-14 23:44:05 +0900

    Avoid unexpected status code

--- .circleci/ci_check.sh
+++ .circleci/ci_check.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PROJECT_DIR=${SCRIPT_DIR}/../
 
 find . -maxdepth 1 -type d \
-       -not -path '*/\.*' \
-       -exec bash -c "digdag check --project {}" \;
+       -not -path '*/\.*' | \
+       xargs -I{} bash -c "digdag check --project {}"
